687100d40c95cb2a1cfc1df109d2ab8c52856412
Dev Grover <Devgrover72@gmail.com>
2024-08-03 17:09:56 +0530

    Update userHandle.js

--- src/redux/userHandle.js
+++ src/redux/userHandle.js
@@ -26,6 +26,7 @@ import {
     updateCurrentUser,
 } from './userSlice';
 
+
 export const authUser = (fields, role, mode) => async (dispatch) => {
     dispatch(authRequest());
 
@@ -35,8 +36,7 @@ export const authUser = (fields, role, mode) => async (dispatch) => {
         });
         if (result.data.role) {
             dispatch(authSuccess(result.data));
-        }
-        else {
+        } else {
             dispatch(authFailed(result.data.message));
         }
     } catch (error) {
@@ -44,12 +44,12 @@ export const authUser = (fields, role, mode) => async (dispatch) => {
     }
 };
 
+// Adds new stuff
 export const addStuff = (address, fields) => async (dispatch) => {
     dispatch(authRequest());
 
     try {
         const result = await axios.post(`${process.env.REACT_APP_BASE_URL}/${address}`, fields, {
-            // SYNTAX ERROR ... IS REMOVED
             headers: { 'Content-Type': 'application/json' },
         });
 
@@ -63,23 +63,19 @@ export const addStuff = (address, fields) => async (dispatch) => {
     }
 };
 
-export const updateStuff = (fields, id, address) => async (dispatch) => {
 
+export const updateStuff = (fields, id, address) => async (dispatch) => {
     try {
-        const result = await axios.put(`${process.env.REACT_APP_BASE_URL}/${address}/${id}`, fields, {
-        
-        });
+        const result = await axios.put(`${process.env.REACT_APP_BASE_URL}/${address}/${id}`, fields);
         if (result.data.message) {
             dispatch(updateFailed(result.data.message));
-        }
-        else {
+        } else {
             dispatch(stuffUpdated());
         }
-
     } catch (error) {
         dispatch(getError(error));
     }
-}
+};
 
 export const deleteStuff = (id, address) => async (dispatch) => {
     dispatch(getRequest());
@@ -94,17 +90,18 @@ export const deleteStuff = (id, address) => async (dispatch) => {
     } catch (error) {
         dispatch(getError(error));
     }
-}
+};
+
 
 export const updateCustomer = (fields, id) => async (dispatch) => {
     dispatch(updateCurrentUser(fields));
     try {
         await axios.put(`${process.env.REACT_APP_BASE_URL}/CustomerUpdate/${id}`, fields);
-        dispatch(stuffUpdated());
     } catch (error) {
         dispatch(getError(error));
     }
 };
+
 
 export const getProductsbySeller = (id) => async (dispatch) => {
     dispatch(getRequest());
@@ -113,14 +110,14 @@ export const getProductsbySeller = (id) => async (dispatch) => {
         const result = await axios.get(`${process.env.REACT_APP_BASE_URL}/getSellerProducts/${id}`);
         if (result.data.message) {
             dispatch(getSellerProductsFailed(result.data.message));
-        }
-        else {
+        } else {
             dispatch(sellerProductSuccess(result.data));
         }
     } catch (error) {
         dispatch(getError(error));
     }
-}
+};
+
 
 export const getProducts = () => async (dispatch) => {
     dispatch(getRequest());
@@ -129,14 +126,14 @@ export const getProducts = () => async (dispatch) => {
         const result = await axios.get(`${process.env.REACT_APP_BASE_URL}/getProducts`);
         if (result.data.message) {
             dispatch(getProductsFailed(result.data.message));
-        }
-        else {
+        } else {
             dispatch(productSuccess(result.data));
         }
     } catch (error) {
         dispatch(getError(error));
     }
-}; //SYNTAX ERROR for ;
+};
+
 
 export const getProductDetails = (id) => async (dispatch) => {
     dispatch(getRequest());
@@ -145,48 +142,45 @@ export const getProductDetails = (id) => async (dispatch) => {
         const result = await axios.get(`${process.env.REACT_APP_BASE_URL}/getProductDetail/${id}`);
         if (result.data.message) {
             dispatch(getProductDetailsFailed(result.data.message));
-        }
-        else {
+        } else {
             dispatch(productDetailsSuccess(result.data));
         }
-
     } catch (error) {
         dispatch(getError(error));
     }
-}
+};
+
 
 export const getCustomers = (id) => async (dispatch) => {
     dispatch(getRequest());
 
     try {
-        const result = await axios.get(`${process.env.REACT_APP_BASE_URL}/${address}/${id}`);
+        const result = await axios.get(`${process.env.REACT_APP_BASE_URL}/getCustomers/${id}`);
         if (result.data.message) {
             dispatch(getCustomersListFailed(result.data.message));
-        }
-        else {
+        } else {
             dispatch(customersListSuccess(result.data));
         }
-
     } catch (error) {
         dispatch(getError(error));
     }
-}
+};
+
 
 export const getSpecificProducts = (id, address) => async (dispatch) => {
     dispatch(getRequest());
+
     try {
         const result = await axios.get(`${process.env.REACT_APP_BASE_URL}/${address}/${id}`);
         if (result.data.message) {
             dispatch(getSpecificProductsFailed(result.data.message));
-        }
-        else {
+        } else {
             dispatch(specificProductSuccess(result.data));
         }
-
     } catch (error) {
         dispatch(getError(error));
     }
-}
+};
 
 export const getSearchedProducts = (address, key) => async (dispatch) => {
     dispatch(getRequest());
@@ -195,12 +189,10 @@ export const getSearchedProducts = (address, key) => async (dispatch) => {
         const result = await axios.get(`${process.env.REACT_APP_BASE_URL}/${address}/${key}`);
         if (result.data.message) {
             dispatch(getSearchFailed(result.data.message));
+        } else {
+            dispatch(setFilteredProducts(result.data));
         }
-        else {
-            dispatch(setFilteredProducts(result.files));
-        }
-
     } catch (error) {
         dispatch(getError(error));
     }
-}
+};
